c98b3609caf8ad930f04eb866e8c513d288fb374
Toan Tran Cong <22110248@student.hcmute.edu.vn>
2023-08-25 21:48:55 +0700

    first commit

--- client/index.js
+++ client/index.js
@@ -1,14 +1,18 @@
-const axios = require('axios');
-const niceList = require('../utils/niceList.json');
-const MerkleTree = require('../utils/MerkleTree');
+const axios = require("axios");
+const niceList = require("../utils/niceList.json");
+const MerkleTree = require("../utils/MerkleTree");
 
-const serverUrl = 'http://localhost:1225';
+const serverUrl = "http://localhost:1225";
 
 async function main() {
-  // TODO: how do we prove to the server we're on the nice list? 
-
+  // TODO: how do we prove to the server we're on the nice list?
+  const tree = new MerkleTree(niceList);
+  let name = "Robin Hessel Jr.";
+  let index = niceList.findIndex((n) => n === name);
   const { data: gift } = await axios.post(`${serverUrl}/gift`, {
     // TODO: add request body parameters here!
+    proof: tree.getProof(index),
+    leaf: name,
   });
 
   console.log({ gift });
